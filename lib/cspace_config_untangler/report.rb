54bdb56219b86d3bf273d41b8d51a08cb0cfb7ca
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-18 18:02:48 -0500

    add CCU::Reports.simplify_allfields method

--- lib/cspace_config_untangler/report.rb
+++ lib/cspace_config_untangler/report.rb
@@ -3,5 +3,13 @@
 module CspaceConfigUntangler
   # Namespace for report generators
   module Report
+    module_function
+
+    def simplify_allfields(row)
+      %w[fid namespace namespace_for_id field_id].each do |field|
+        row.delete(field)
+      end
+      row
+    end
   end
 end
