746bc91143a6c7828388b53d412bd6391bb5ce21
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-18 19:38:24 -0500

    add Report.reference_reports method to bundle standard release report creation

--- lib/cspace_config_untangler/report.rb
+++ lib/cspace_config_untangler/report.rb
@@ -1,9 +1,34 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 module CspaceConfigUntangler
   # Namespace for report generators
   module Report
     module_function
+
+    def reference_reports(release)
+      dir = CCU.data_reference_dir(release: release)
+      FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
+
+      CCU::Report::AllFieldsGenerator.call(release: release)
+      CCU::Report::AllFieldsGenerator.call(
+        release: release,
+        datemode: :collapsed
+      )
+      CCU::Report::MultiAuthRepeatableFieldsGenerator.call(
+        release: release
+      )
+      CCU::Report::StructuredDateFieldsGenerator.call(
+        release: release
+      )
+      CCU::Cli::Helpers::ProfileGetter.call('all').each do |profile|
+        CCU::Report::ProfileFieldsGenerator.call(
+          release: release,
+          profile: profile
+        )
+      end
+    end
 
     def simplify_allfields(row)
       %w[fid namespace namespace_for_id field_id].each do |field|
