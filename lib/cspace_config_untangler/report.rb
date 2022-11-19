e1ae5dc3540e4139b9bc62c26c2b4bc98af8be57
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-18 21:11:23 -0500

    add qa all fields report

--- lib/cspace_config_untangler/report.rb
+++ lib/cspace_config_untangler/report.rb
@@ -30,6 +30,20 @@ module CspaceConfigUntangler
       end
     end
 
+    def deversion_for_qa(row)
+      row['fid'] = row['fid'].sub(/_\d+(-\d+){2} /, ' ')
+      row['profile'] = row['profile'].sub(/_\d+(-\d+){2}/, '')
+      row
+    end
+
+    def get_qa_table(release)
+      vrelease = CCU::Validate.release(release)
+      path = CCU.allfields_path(release: vrelease)
+
+      CSV.parse(File.read(path), headers: true)
+        .map{ |row| deversion_for_qa(row) }
+    end
+
     def simplify_allfields(row)
       %w[fid namespace namespace_for_id field_id].each do |field|
         row.delete(field)
