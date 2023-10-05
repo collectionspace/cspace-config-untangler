76f693ae73727a8ed709f0213b86c529f3fd9bbf
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-10-05 11:37:13 -0400

    Add 7.2 dev mapper manifest

--- lib/cspace_config_untangler/manifest_dev.rb
+++ lib/cspace_config_untangler/manifest_dev.rb
@@ -1,21 +1,23 @@
-require 'cspace_config_untangler/manifest'
+require "cspace_config_untangler/manifest"
 
 module CspaceConfigUntangler
   class ManifestDev < CCU::Manifest
-
     MapperPath = Struct.new(:profile, :rectype, :path)
 
     DEV_MAPPERS = {
-        'anthro' => %w[collectionobject concept-associated],
-        'fcart' => %w[collectionobject concept-associated],
-        'core' => %w[collectionobject concept-associated nonhierarchicalrelationship objecthierarchy]
-      }
-    
+      "anthro" => %w[collectionobject concept-associated
+        chronology-field-collection],
+      "fcart" => %w[collectionobject concept-associated],
+      "core" => %w[collectionobject concept-associated
+        chronology-event nonhierarchicalrelationship
+        objecthierarchy]
+    }
+
     private
 
     def create_mapper_path(path)
-      splitpath = path.split('/')
-      rectype = splitpath.pop.delete_suffix('.json').split('_').pop
+      splitpath = path.split("/")
+      rectype = splitpath.pop.delete_suffix(".json").split("_").pop
       profile = splitpath.pop
       MapperPath.new(profile, rectype, path)
     end
@@ -26,17 +28,17 @@ module CspaceConfigUntangler
 
       true
     end
-        
+
     def mapper_paths
-      mapper_paths_all.map{ |path| create_mapper_path(path) }
-        .select{ |mapper| included?(mapper) }
-        .map{ |mapper| mapper.path }
+      mapper_paths_all.map { |path| create_mapper_path(path) }
+        .select { |mapper| included?(mapper) }
+        .map { |mapper| mapper.path }
     end
 
     def mapper_paths_all
       return Dir.glob("#{indir}/**/*.json") if recurse
 
-      indir.children.select{ |child| child.extname == '.json' }
+      indir.children.select { |child| child.extname == ".json" }
     end
   end
 end
