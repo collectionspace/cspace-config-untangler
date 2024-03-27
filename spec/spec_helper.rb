8f3e791cee98e2028bc2d2fc3624c199c65da8af
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-03-27 11:28:34 -0400

    Implement Release class
    Replaces doing a bunch of weird stuff in the base module/settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.after(:each) { CCU.reset_config }
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
