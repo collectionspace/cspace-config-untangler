a624dfa72eab2b437590fb94a66fc32a6c94ba1e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-05-10 18:59:38 -0400

    Fix missed conflict

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
-<<<<<<< HEAD
-=======
 
 require "bundler/setup"
 require "rspec/custom/matchers/match_csv"
->>>>>>> a034c3944f7ed1a798cdcb8335753b901ed1956d
 
-require 'bundler/setup'
-require 'rspec/custom/matchers/match_csv'
-
-require 'cspace_config_untangler'
-require_relative 'helpers'
-require 'dry/configurable/test_interface'
+require "cspace_config_untangler"
+require_relative "helpers"
+require "dry/configurable/test_interface"
 
 module CspaceConfigUntangler
   enable_test_interface
@@ -20,7 +14,7 @@ end
 RSpec.configure do |config|
   config.include Helpers
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+  config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
