4af8c3896ca0c1aa86bdebdad8d9c13d93f200c4
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-05-15 21:12:19 -0400

    Capture field definition `[config]` keys; Refactor HashIterator

--- lib/cspace_config_untangler/fields/definition/hash_iterator.rb
+++ lib/cspace_config_untangler/fields/definition/hash_iterator.rb
@@ -16,40 +16,51 @@ module CspaceConfigUntangler
         #   CCU::Fields::Definition::Grouping]
         def initialize(config, called_from)
           @config = config
-          clean_config_hash
           @caller = called_from
           @typer = HashEntryTyper.new(@config)
-          parse_fields
+          process
         end
 
         private
 
-        def clean_config_hash
-          old_hash = @config.hash.dup
-          return if old_hash.length == 1
+        attr_reader :config, :caller, :typer, :namespace
 
-          cleaned_hash = old_hash.except("[config]")
-          @config.update_field_hash(cleaned_hash)
+        def process
+          capture_config_key
+          delete_config_key if config.hash.size > 1
+          parse_fields
         end
 
-        def namespace
-          @config.namespace.literal
+        def capture_config_key
+          configkey = "[config]"
+          return unless config.hash.key?(configkey)
+
+          config.parser.add_config_key(config, config.hash[configkey])
+        end
+
+        def delete_config_key
+          configkey = "[config]"
+          return unless config.hash.key?(configkey)
+
+          working = CCU.safe_copy(config.hash)
+          working.delete(configkey)
+          config.update_field_hash(working)
         end
 
         def parse_fields
-          @config.hash.each { |name, data| parse_field(name, data) }
+          config.hash.each { |name, data| parse_field(name, data) }
         end
 
         def parse_field(name, data)
-          type = @typer.call(data)
+          type = typer.call(data)
           return unless type
 
-          config = @config.derive_child(name: name, field_hash: data,
-            parent: @caller)
+          child = config.derive_child(name: name, field_hash: data,
+            parent: caller)
           if type == :field || type == :structured_date
-            FieldFilter.call(config)
+            FieldFilter.call(child)
           elsif type == :group
-            Grouping.new(config)
+            Grouping.new(child)
           end
         end
       end
