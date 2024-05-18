53ea20ee2642b80e721cdc5e9c9b4956b2bf7e74
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-05-18 15:27:32 -0400

    Add extraction of ns_for_id from [config] hash

--- spec/cspace_config_untangler/fields/definition/namespace_spec.rb
+++ spec/cspace_config_untangler/fields/definition/namespace_spec.rb
@@ -3,11 +3,13 @@
 require "spec_helper"
 
 RSpec.describe CCU::Fields::Def::Namespace do
-  let(:ns) { CCU::Fields::Def::Namespace.new(str) }
+  let(:ns) { CCU::Fields::Def::Namespace.new(str, hash) }
   let(:str) { "ns2:collectionobjects_common" }
+  let(:hash) { nil }
 
   describe "#literal" do
     let(:result) { ns.literal }
+
     it "returns literal namespace from rectype/fields/document config" do
       expect(result).to eq(str)
     end
@@ -16,16 +18,58 @@ RSpec.describe CCU::Fields::Def::Namespace do
   describe "#for_id" do
     let(:result) { ns.for_id }
 
-    context "when same as literal namespace" do
-      it "returns literal namespace" do
-        expect(result).to eq(str)
+    it "returns literal namespace when no alternate provided" do
+      expect(result).to eq(str)
+    end
+
+    context "when literal lookup namespace" do
+      let(:str) { "ns2:propagations_common" }
+
+      it "returns adjusted namespace" do
+        expect(result).to eq("ns2:propagation_common")
       end
     end
 
-    context "when adjusted namespace" do
-      let(:str) { "ns2:propagations_common" }
+    context "when extensionName lookup namespace" do
+      let(:str) { "ns2:chronologies_common" }
+      let(:hash) do
+        {"[config]" => {
+          "view" => {"type" => "CompoundInput"},
+          "extensionName" => "associatedAuthority",
+          "extensionParentConfig" => {
+            "service" => {
+              "ns" => "http://collectionspace.org/services/chronology"
+            }
+          }
+        }}
+      end
+
       it "returns adjusted namespace" do
-        expect(result).to eq("ns2:propagation_common")
+        expect(result).to eq("ext.associatedAuthority")
+      end
+    end
+
+    context "when message name lookup namespace" do
+      let(:str) { "ns2:chronologies_common" }
+      let(:hash) do
+        {"[config]" =>
+         {"messages" =>
+          {"fullName" => {
+             "id" => "field.ext.associatedAuthority.assocPerson.fullName",
+             "defaultMessage" => "Associated person"
+           },
+           "name" => {
+             "id" => "field.ext.associatedAuthority.assocPerson.name",
+             "defaultMessage" => "Person"
+           }},
+          "view" => {
+            "type" => "AutocompleteInput",
+            "props" => {"source" => "person/local,person/ulan"}
+          }}}
+      end
+
+      it "returns adjusted namespace" do
+        expect(result).to eq("ext.associatedAuthority")
       end
     end
   end
