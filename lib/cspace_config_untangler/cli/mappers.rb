6f38c774c2168d4a44e6fdc7cc09a13fa1466ee7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-08-22 14:43:22 -0400

    noblame: Whitespace and/or formatting changes only

--- lib/cspace_config_untangler/cli/mappers.rb
+++ lib/cspace_config_untangler/cli/mappers.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Layout/LineLength
 require "digest"
 
 require_relative "subcommand_base"
@@ -23,27 +22,22 @@ module CspaceConfigUntangler
       desc "manifest",
         "Writes JSON manifest of RecordMappers, as used by cspace-batch-import"
       long_desc <<~LONGDESC
-        Usage:
-        \x5  ccu mappers manifest
-
-        Options:
-        \x5  -i, [--inputdir=INPUTDIR]    # Path to directory containing RecordMapper JSON files. Specify the path relative to #{CCU.mapperdir}
-        \x5  -o, [--output=OUTPUT]        # Path to output file
-        \x5                               # Default: /Users/kristina/code/cspace-config-untangler/data/mappers.json
-        \x5  -r, [--recursive=RECURSIVE]  # y/n. Whether to traverse the inputdir recursively
-        \x5                               # Default: y
-
-        Includes valid mappers in the given directory (recursively, as an option). Invalid mappers are excluded
+        Includes valid mappers in the given directory (recursively, as an
+        option). Invalid mappers are excluded
 
         The manifest is used by cspace-batch-import.
 
-        Assumes that all mappers will be found in `#{CCU.mapperdir}` (CCU.mapperdir) or subdirectories thereof. Base URI for raw files in this directory on Github in CCU.mapper_uri_base: #{CCU.mapper_uri_base}
+        Assumes that all mappers will be found in `#{CCU.mapperdir}`
+        (CCU.mapperdir) or subdirectories thereof. Base URI for raw files in
+        this directory on Github in CCU.mapper_uri_base: #{CCU.mapper_uri_base}
 
-        These constants can be changed in `lib/cspace_config_untangler.rb` if necessary.
+        These constants can be changed in `lib/cspace_config_untangler.rb` if
+        necessary.
       LONGDESC
       option(:inputdir,
         type: :string,
-        desc: "Path to directory containing RecordMapper JSON files. Specify the path relative to #{CCU.mapperdir}",
+        desc: "Path to directory containing RecordMapper JSON files. Specify "\
+          "the path relative to #{CCU.mapperdir}",
         default: "",
         aliases: "-i")
       option(:output,
@@ -73,22 +67,38 @@ module CspaceConfigUntangler
                 recursive: options[:recursive]}
         opts.each { |key, val| puts "  #{key}: #{val}" }
         puts "  dev: #{options[:dev]}"
-        builder = options[:dev] ? CCU::ManifestDev.new(**opts) : CCU::Manifest.new(**opts)
+        builder = if options[:dev]
+          CCU::ManifestDev.new(**opts)
+        else
+          CCU::Manifest.new(**opts)
+        end
         builder.build
       end
 
       desc "write",
-        "Writes JSON serializations of RecordMappers for the given rectype(s) for the given profiles."
+        "Writes JSON serializations of RecordMappers for the given rectype(s) "\
+      "for the given profiles."
       option :outputdir,
-        desc: "Path to output directory. File name will be: profile-rectype.json", default: "data/mappers", aliases: "-o"
+        desc: "Path to output directory. File name will be: "\
+        "profile-rectype.json",
+        default: "data/mappers",
+        aliases: "-o"
       option :subdirs,
-        desc: "y/n. Whether to organize into subdirectories within given output directory by normalized profile name. Normalized profile name is the profile with version info/underscores removed.", default: "n", aliases: "-s"
+        desc: "y/n. Whether to organize into subdirectories within given "\
+        "output directory by normalized profile name. Normalized profile name "\
+        "is the profile with version info/underscores removed.",
+        default: "n",
+        aliases: "-s"
       def write
         get_profiles.each do |profile|
           puts "Writing mappers for #{profile}..."
           p = CCU::Profile.new(profile: profile, rectypes: parse_rectypes,
             structured_date_treatment: :collapse)
-          dir_path = (options[:subdirs] == "y") ? "#{options[:outputdir]}/#{p.basename}" : options[:outputdir]
+          dir_path = if options[:subdirs] == "y"
+            "#{options[:outputdir]}/#{p.basename}"
+          else
+            options[:outputdir]
+          end
           FileUtils.mkdir_p(dir_path)
           p.rectypes.each do |rt|
             puts "  ...#{rt.name}"
@@ -109,28 +119,31 @@ module CspaceConfigUntangler
         end
       end
 
-      desc "validate",
-        "Prints to screen a validation report of the JSON mappers in a directory"
+      desc "validate", "Prints to screen a validation report of the JSON "\
+        "mappers in a directory"
       long_desc <<-LONGDESC
-    The output directory given will be recursively traversed to find .json files. It is
-      expected that all .json files in this directory structure will be RecordMappers.
+        The output directory given will be recursively traversed to find .json
+        files. It is expected that all .json files in this directory structure
+        will be RecordMappers.
 
-    Currently the validation checks for:
-      - expected top-level keys
-      - a URI for every namespace defined for the Mapper
-      - a namespace defined for every field mapping
+        Currently the validation checks for:
+          - expected top-level keys
+          - a URI for every namespace defined for the Mapper
+          - a namespace defined for every field mapping
       LONGDESC
-      option :input,
-        desc: "Path to input directory containing JSON mappers. Will be traversed recursively", default: "data/mappers", aliases: "-i"
+      option :input, desc: "Path to input directory containing JSON mappers. "\
+        "Will be traversed recursively",
+        default: "data/mappers",
+        aliases: "-i"
       def validate
         mapper_paths = Dir.glob("#{options[:input]}/**/*.json")
         mapper_paths.each do |path|
           validator = RecordMapper::Validator.new(path)
           validator.report
         end
-        puts "\n\nValidation complete. Any errors were reported above. Nothing above means all are valid!"
+        puts "\n\nValidation complete. Any errors were reported above. "\
+          "Nothing above means all are valid!"
       end
     end
   end
 end
-# rubocop:enable Layout/LineLength
