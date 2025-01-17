04581ac98324063805698d3d57d419c65521a3c3
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-01-17 16:48:18 -0500

    Implement automated config fetching for community profiles

---
+++ lib/cspace_config_untangler/ui_config.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "json"
+
+module CspaceConfigUntangler
+  module UiConfig
+    module_function
+
+    def fetch_community_profiles(release: CCU.release.version,
+      env: CCU.instance_env)
+      CCU.config.release = release unless CCU.release.version == release
+      CCU.config.instance_env = env
+      CCU.community_supported_profiles.each do |profile|
+        CCU::ConfigFetcher.call(profile)
+      end
+    end
+
+    # @param path [String] to JSON config
+    def reformat(path)
+      json = JSON.parse(File.read(path))
+      File.open(path, "w") do |outfile|
+        outfile.puts(JSON.pretty_generate(json))
+      end
+    end
+  end
+end
