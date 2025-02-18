collectionspace/cspace-config-untangler

commit 764008aff42ef4ab371f3124d12b70bdb9fb127e

Get RecordType namespace from first advancedSearch value
Across record types, neither documentName nor servicePath is
consistently appended to "_common" to create the base namespace of a
record type.

The first field in the advancedSearch screen for a record type is
always the identifier field, which is always in the base namespace.

lib/cspace_config_untangler/record_type.rb
@@ -149,24 +149,28 @@ module CspaceConfigUntangler
     def id_field
       case @service_type
       when "object"
-        id_field = "objectNumber"
+        "objectNumber"
       when "authority"
-        id_field = "shortIdentifier"
+        "shortIdentifier"
       when "procedure"
         required_mappings = batch_mappings.select { |m| m.required == "y" }
         case required_mappings.length
         when 0
-          id_field = "potTagNumber" if @name == "pottag"
+          case name
+          when "loanout" then "loanOutNumber"
+          when "pottag" then "potTagNumber"
+          end
         when 1
-          id_field = required_mappings.first.fieldname
+          required_mappings.first.fieldname
         else
+          case name
+          when "movement" then "movementReferenceNumber"
           # osteology has 3 required fields, but only the ID is suitable for use
           # here
-          id_field = "InventoryID" if @name == "osteology"
-          id_field = "movementReferenceNumber" if @name == "movement"
+          when "osteology" then "InventoryID"
+          end
         end
       end
-      id_field
     end
 
     def search_field
@@ -368,10 +372,7 @@ module CspaceConfigUntangler
     end
 
     def get_namespace
-      return "ns2:dutiesofcare_common" if name == "dutyofcare"
-      return "ns2:propagations_common" if name == "propagation"
-
-      "ns2:#{service_config.document_name.downcase}_common"
+      config.dig("advancedSearch", "value", 0, "path")&.split("/")&.first
     end
   end
 end

spec/cspace_config_untangler/record_mapper/record_mapping_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe CCU::RecordMapper::RecordMapping do
   end
   let(:profilename) { "core" }
   let(:rectypes) { ["collectionobject", "concept", "movement"] }
-  let(:release) { "6_1" }
+  let(:release) { "8_1" }
   let(:profile) { generator.profile }
 
   describe RecordMapping do

commit 8492f1caf6fef244f607f7e70172ab26fb078a7b

Fix namespace assignment bug introduced by commit 764008a

lib/cspace_config_untangler/record_type.rb
@@ -167,8 +167,8 @@ module CspaceConfigUntangler
         else
           case name
           when "movement" then "movementReferenceNumber"
-          # osteology has 3 required fields, but only the ID is suitable for use
-          # here
+            # osteology has 3 required fields, but only the ID is
+            # suitable for use here
           when "osteology" then "InventoryID"
           end
         end
@@ -374,7 +374,15 @@ module CspaceConfigUntangler
     end
 
     def get_namespace
-      config.dig("advancedSearch", "value", 0, "path")&.split("/")&.first
+      return "ns2:contacts_common" if name == "contact"
+
+      value_idx = if profile.basename == "botgarden" && name == "loanout"
+        1
+      else
+        0
+      end
+      config.dig("advancedSearch", "value", value_idx, "path")&.split("/")
+        &.first
     end
   end
 end

spec/cspace_config_untangler/forms/props_spec.rb
@@ -13,8 +13,11 @@ RSpec.describe CCU::Forms::Props do
   let(:profilename) { "anthro" }
   let(:rectypes) { ["collectionobject"] }
   let(:generator) do
-    Helpers::SetupGenerator.new(profile: profilename, rectypes:,
-      release:)
+    Helpers::SetupGenerator.new(
+      profile: profilename,
+      rectypes: rectypes,
+      release: release
+    )
   end
   let(:templatename) { "default" }
   let(:form) { generator.form(templatename) }
@@ -239,6 +242,17 @@ RSpec.describe CCU::Forms::Props do
 
       it "returns correct ns" do
         expect(result).to eq("ext.accessionattributes")
+      end
+    end
+
+    context "with botgarden loanOutNumber (8.1)" do
+      let(:release) { "8_1" }
+      let(:profilename) { "botgarden" }
+      let(:rectypes) { ["loanout"] }
+      let(:name) { "loanOutNumber" }
+
+      it "returns correct ns" do
+        expect(result).to eq("ns2:loansout_common")
       end
     end
 

spec/cspace_config_untangler/record_type_spec.rb
@@ -149,6 +149,29 @@ RSpec.describe CCU::RecordType do
     end
   end
 
+  describe ".ns" do
+    let(:result) { rectype.ns }
+    let(:release) { "8_1" }
+
+    context "when botgarden profile" do
+      let(:profilename) { "botgarden" }
+
+      context "when collectionobject" do
+        it "returns correct namespace" do
+          expect(result).to eq("ns2:collectionobjects_common")
+        end
+      end
+
+      context "when loanout" do
+        let(:rectypes) { ["loanout"] }
+
+        it "returns correct namespace" do
+          expect(result).to eq("ns2:loansout_common")
+        end
+      end
+    end
+  end
+
   describe "#mappings" do
     let(:result) { rectype.mappings }
     let(:columns) do