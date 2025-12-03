dcdb6f24743e9c362650054daa2af8e4239645fc
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-12-02 20:00:57 -0500

    test: Reformat IterativeFieldExtractor spec (in dev)

--- spec/cspace_config_untangler/forms/iterative_field_extractor_spec.rb
+++ spec/cspace_config_untangler/forms/iterative_field_extractor_spec.rb
@@ -1,26 +1,26 @@
-# frozen_string_literal: true
+#frozen_string_literal: true
 
-# require 'spec_helper'
+require 'spec_helper'
 
-# RSpec.describe CCU::Forms::IterativeFieldExtractor do
-#   subject(:iterator) { described_class.new(form, formconfig) }
+RSpec.describe CCU::Forms::IterativeFieldExtractor do
+  # subject(:iterator) { described_class.new(form, formconfig) }
 
-#   let(:release) { '8_0' }
-#   let(:profilename) { 'anthro' }
-#   let(:rectypes) { ['media'] }
-#   let(:generator) do
-#     Helpers::SetupGenerator.new(profile: profilename, rectypes:,
-#                                 release:)
-#   end
-#   let(:form) { generator.form }
-#   let(:formconfig) { form.field_config }
+  # let(:release) { '8_2' }
+  # let(:profilename) { 'anthro' }
+  # let(:rectypes) { ['collectionobject'] }
+  # let(:generator) do
+  #   Helpers::SetupGenerator.new(profile: profilename, rectypes:,
+  #                               release:)
+  # end
+  # let(:form) { generator.form }
+  # let(:formconfig) { form.field_config }
 
-#   describe 'subrecord' do
-#     context 'when prop represents a form panel' do
+  # describe 'subrecord' do
+  #   context 'when prop represents a form panel' do
 
-#       it 'returns true' do
-#         iterator.call
-#       end
-#     end
-#   end
-# end
+  #     it 'returns true' do
+  #       iterator.call
+  #     end
+  #   end
+  # end
+end
