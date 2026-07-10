6ae9415c68655d966151306fb5cab83c8f63bdaf
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-07-10 18:36:37 -0400

    feat: Produce Data Toolkit-style mappings for rectypes

--- lib/cspace_config_untangler/field_map/field_mapper.rb
+++ lib/cspace_config_untangler/field_map/field_mapper.rb
@@ -6,21 +6,21 @@ module CspaceConfigUntangler
     # which corresponds to an incoming data key/column)
     class FieldMapper
       ::FieldMapper = CspaceConfigUntangler::FieldMap::FieldMapper
-      attr_reader :mappings
+
+      # @param field [CCU::Fields::Field]
+      # @param column_style %i[fully_consistent consistent data_toolkit fancy]
       def initialize(field:, column_style: :fully_consistent)
         @field = field
-        @sources = field.value_sources
         @column_style = column_style
-        add_refname_source
-        @fieldhash = populate_hash
-        # @hash = structure_hash
-        # get_data_columns
-        # get_source_types
-        # get_transforms
-        @mappings = create_mappings
       end
 
-      def hash = fieldhash
+      def sources = @sources ||= derive_sources
+
+      def columns = @columns ||= derive_columns
+
+      def mappings = @mappings ||= create_mappings
+
+      def hash = columns
 
       def source_type = @source_type ||= get_source_type
 
@@ -29,15 +29,55 @@ module CspaceConfigUntangler
 
       private
 
-      attr_reader :field, :sources, :column_style, :fieldhash
+      attr_reader :field, :column_style
 
-      def populate_hash
-        sources.map { |source| populate_source(source) }
-          .to_h
+      def derive_sources
+        return field.value_sources if column_style == :data_toolkit
+
+        refname_source_added
       end
 
-      def populate_source(source)
-        [source, source_hash(source)]
+      def derive_columns
+        return derive_data_toolkit_columns if column_style == :data_toolkit
+
+        populate_columns
+      end
+
+      def derive_data_toolkit_columns
+        return populate_columns unless sources.first.source_type == "authority"
+
+        src = field.value_sources.first
+        vocabcol = CCU::ValueSources::DataToolkitAuthVocab.new
+        {
+          src =>
+            data_toolkit_authority_value_column(src),
+          vocabcol => data_toolkit_authority_vocab_column
+        }
+      end
+
+      def data_toolkit_authority_value_column(source)
+        base = {
+          column_name: field.name,
+          source_type: "authority",
+          source_name: nil,
+          transforms: {}
+        }
+
+        append_transforms(base, source)
+      end
+
+      def data_toolkit_authority_vocab_column
+        {
+          column_name: "#{field.name}AuthorityVocabulary",
+          source_type: "authority vocabulary indication",
+          source_name: nil,
+          transforms: {}
+        }
+      end
+
+      def populate_columns
+        sources.map { |source| [source, source_hash(source)] }
+          .to_h
       end
 
       def get_source_type
@@ -54,7 +94,13 @@ module CspaceConfigUntangler
           source_name: source.name,
           transforms: {}
         }
+
+        append_transforms(base, source)
+      end
+
+      def append_transforms(base, source)
         return base if source.source_type == "refname"
+
         base[:transforms].merge!(transforms) if transforms
         base[:transforms].merge!(source.transforms) if source.transforms
         base
@@ -77,17 +123,21 @@ module CspaceConfigUntangler
           end
         when :fancy
           source.column_header_fancy(field.name, sources)
+        else
+          field.name
         end
       end
 
-      def add_refname_source
-        src = sources.first
-        return unless needs_refname_source?(src)
+      def refname_source_added
+        srcs = field.value_sources
+        src = srcs.first
+        return srcs unless needs_refname_source?(src)
 
-        @sources << CCU::ValueSources::Refname.new(src)
+        srcs << CCU::ValueSources::Refname.new(src)
+        srcs
       end
 
-      # @param source [#source_type]
+      # @param sources [#source_type]
       def needs_refname_source?(source)
         %w[authority vocabulary].include?(source.source_type)
       end
@@ -103,11 +153,11 @@ module CspaceConfigUntangler
       end
 
       def create_mappings
-        fieldhash.map do |source, h|
+        columns.map do |source, h|
           if source.source_type == "authority" && !source.configured?
             nil
           else
-            FieldMapping.new(field: @field,
+            FieldMapping.new(field: field,
               datacolumn: h[:column_name],
               transforms: h[:transforms],
               source_type: h[:source_type],
